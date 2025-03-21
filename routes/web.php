0adf8350bf13fbd44414ece9b4153230255574f7
Ernar533 <ernarik533@gmail.com>
2025-03-21 23:26:50 +0500

    Добавил все файлы

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Main\IndexController as MainIndexController;
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
+use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexController;
 
+// Маршрут для главной страницы
 Route::group(['namespace' => 'Main'], function () {
-    Route::get('/', [IndexController::class, '__invoke']);
+    Route::get('/', [MainIndexController::class, '__invoke']);
 });
+
+// Группа маршрутов для админки
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', [AdminIndexController::class, '__invoke']);
+    });
+
+    Route::group(['namespace' => 'Category','prefix' =>"categories"], function () {
+        Route::get('/', [CategoryIndexController::class, '__invoke'])->name('category.index');
+    });
+});
+// Аутентификация
 Auth::routes();
